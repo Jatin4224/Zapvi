710417c1d073e50e1047630484dc412d9b7f507d
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-04-09 05:50:48 +0530

    .

--- zapvi/src/components/ui/Reviews.tsx
+++ zapvi/src/components/ui/Reviews.tsx
@@ -24,11 +24,40 @@ function splitArray<T>(array: Array<T>, numParts: number) {
   }
   return result;
 }
-
+function ReviewColumn({
+  reviews,
+  className,
+  reviewClassName,
+  msPerPixel = 0,
+}: {
+  reviews: string[];
+  className?: string;
+  reviewClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}) {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  return <div></div>;
+}
 function ReviewGrid() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(containerRef, { once: true, amount: 0.4 });
   const columns = splitArray(PHONES, 3);
+  const column1 = columns[0];
+  const column2 = columns[1];
+  const column3 = splitArray(columns[2], 2);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-col-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? (
+        <>
+          <ReviewColumn />
+        </>
+      ) : null}
+    </div>
+  );
 }
 
 export function Reviews() {
