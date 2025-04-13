3abfe447934dd3476f6b2adcfcb00847948f7009
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-04-14 04:53:42 +0530

    .

--- zapvi/src/app/config/upload/page.tsx
+++ zapvi/src/app/config/upload/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { Progress } from "@/components/ui/progress";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import {
+  Image,
+  Loader2,
+  MousePointer,
+  MousePointerSquareDashed,
+} from "lucide-react";
+import { useState, useTransition } from "react";
 import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
@@ -10,7 +17,9 @@ const Page = () => {
   const onDropAccepted = () => {
     console.log("accepted");
   };
-
+  const isUploading = false;
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const [isPending, startTransition] = useTransition();
   return (
     <div
       className={cn(
@@ -38,6 +47,37 @@ const Page = () => {
               {...getRootProps()}
             >
               <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className="h-6 w-6 text-zinc-500 mb-2" />
+              ) : isUploading || isPending ? (
+                <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+              ) : (
+                <Image className="h-6 w-6 text-zinc-500 mb-2" />
+              )}
+              <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading...</p>
+                    <Progress
+                      value={uploadProgress}
+                      className="mt-2 w-40 h-2 bg-gray-300"
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className="font-semibold">Drop file</span> to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                )}
+              </div>
             </div>
           )}
         </Dropzone>
---
+++ zapvi/src/components/ui/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <h1 className="flex justify-center">2024 All rights reserved</h1>;
+};
+
+export default Footer;
